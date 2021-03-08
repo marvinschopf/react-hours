7971753b88279f8ab3b0b4b8e1f4758b0b7063eb
Marvin Schopf <marvin@schopf.biz>
2021-03-08 02:23:13 +0100

    Add example for callback

--- src/index.stories.tsx
+++ src/index.stories.tsx
@@ -34,3 +34,10 @@ export const PredefinedAlwaysOpen = () => <Hours value="24/7" />;
 export const PredefinedComplex = () => (
 	<Hours value="Mo 01:00-04:15,08:00-10:00,16:30-18:15; Tu 17:30-18:30; We 04:15-24:00; Th 00:00-11:00; Fr 02:15-02:30,06:15-09:45; Sa 01:30-01:45,05:30-10:15; Su 17:45-19:00" />
 );
+export const ChangeCallback = () => {
+	<Hours
+		onChange={(val: string) => {
+			console.log(val);
+		}}
+	/>;
+};
